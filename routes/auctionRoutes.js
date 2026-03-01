352c2e956828e305deb57cbb88503a553bd41ba5
Karen Michel <kmpalaciosl@academia.usbbog.edu.co>
2026-02-28 23:10:16 -0500

    controllers

---
+++ routes/auctionRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+const auctionController = require("../controllers/auctionController")
+
+router.post("/", auctionController.createAuction)
+
+router.post("/bid", auctionController.placeBid)
+
+module.exports = router
