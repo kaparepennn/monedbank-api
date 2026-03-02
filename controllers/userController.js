84831436ff6c89cfc008cb58e84456a85a4d2039
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2026-03-02 19:23:29 +0000

    user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,19 @@
-const Auction = require("../models/Auction")
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-exports.createAuction = async (req, res) => {
+exports.register = async (req, res) => {
     try {
-        const auction = new Auction({
-            title: req.body.title,
-            description: req.body.description,
-            initialPrice: req.body.initialPrice,
-            currentPrice: req.body.initialPrice,
-            endDate: req.body.endDate,
-            createdBy: req.body.userId
-        })
-
-        await auction.save()
-
-        res.json({ message: "Subasta creada correctamente", auction })
+        res.json({ message: "Registro funcionando" })
     } catch (error) {
-        res.status(500).json({ message: "Error al crear subasta" })
+        res.status(500).json({ message: "Error en registro" })
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        res.json({ message: "Login funcionando" })
+    } catch (error) {
+        res.status(500).json({ message: "Error en login" })
     }
 }
