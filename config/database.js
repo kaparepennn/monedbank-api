1131bfa144a584099ff0dc82ebe542c259e42e9d
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2026-03-04 19:42:01 +0000

    guardar cambios

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Base de datos conectada correctamente")
+    } catch (error) {
+        console.log("Error al conectar base de datos", error)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
