fd6d68fa1935ad903218771ac87c015aff3038ef
Karen Michel <148507721+kaparepennn@users.noreply.github.com>
2026-03-04 19:01:20 +0000

    guardar models

--- models/User.js
+++ models/User.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     balance: {
         type: Number,
         default: 0
     }
 })
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", userSchema)
